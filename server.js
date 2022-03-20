808be2916e2c7ee86f48eb0378401d12c7caf0b1
Dan <dan@Ds-MacBook-Pro.local>
2022-03-19 22:26:35 -0600

    better this way

--- server.js
+++ server.js
@@ -1,82 +1,82 @@
 const express = require('express');
 const cors = require('cors');
-const db = require('./app/models');
+const { ServerApiVersion } = require('mongodb');
+const { default: mongoose } = require('mongoose');
+const authRoutes = require('./routes/authRoutes');
+// const routes = require('./app/routes');
 
 // Point towards the config file for mongodb. TODO: Can switch to ENV vars later
-const dbConfig = require('./app/config/db.config');
+// const dbConfig = require('./app/config/db.config');
 
-const Role = db.role;
 const app = express();
 
-// ********** Routes **********
-require('./app/routes/auth.routes');
-require('./app/routes/user.routes');
+/* ********** Middleware ********** */
+app.use(express.static('public'));
 
-// ********** Cors **********
-const corsOptions = {
-  origin: 'http://localhost:8081',
-};
+/* ********** View engine ********** */
+// app.set('view engine', 'ejs');
 
-// set the origin on port 8081
-app.use(cors(corsOptions));
-// requests will have the content type of application/json
-app.use(express.json());
-// read requests using application/x-www-form-urlencoded
-app.use(express.urlencoded({ extended: true }));
-
-// Baby's first route
-app.get('/', (req, res) => {
-  res.json({ message: 'your first API Call to your cool backend' });
-});
-
-// App will listen on port 8080 or whatever the environment decides for the port
-const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {
-  console.log(`Dan's Server is running on port ${PORT}`);
-});
-
-// ********** MongoDB **********
+/* ********** MongoDB ********** */
 // seed the database with initial data
-function initial() {
-  Role.estimatedDocumentCount((err, count) => {
-    if (!err && count === 0) {
-      new Role({
-        name: 'user',
-      }).save((userErr) => {
-        if (userErr) {
-          console.log('error', userErr);
-        }
-        console.log("added 'user' to roles collection");
-      });
-      new Role({
-        name: 'moderator',
-        // eslint-disable-next-line no-shadow
-      }).save((err) => {
-        if (err) {
-          console.log('error', err);
-        }
-        console.log('added "moderator" to the roles collection');
-      });
-      new Role({
-        name: 'admin',
-        // eslint-disable-next-line no-shadow
-      }).save((err) => {
-        if (err) {
-          console.log('error', err);
-        }
-        console.log('added "admin" to the roles collection');
-      });
-    }
-  });
-}
+// function initial() {
+//   Role.estimatedDocumentCount((err, count) => {
+//     if (!err && count === 0) {
+//       new Role({
+//         name: 'user',
+//       }).save((userErr) => {
+//         if (userErr) {
+//           console.log('error', userErr);
+//         }
+//         console.log("added 'user' to roles collection");
+//       });
+//       new Role({
+//         name: 'moderator',
+//         // eslint-disable-next-line no-shadow
+//       }).save((err) => {
+//         if (err) {
+//           console.log('error', err);
+//         }
+//         console.log('added "moderator" to the roles collection');
+//       });
+//       new Role({
+//         name: 'admin',
+//         // eslint-disable-next-line no-shadow
+//       }).save((err) => {
+//         if (err) {
+//           console.log('error', err);
+//         }
+//         console.log('added "admin" to the roles collection');
+//       });
+//     }
+//   });
+// }
 
-db.mongoose
-  .connect(`mongodb://${dbConfig.HOST}:${dbConfig.PORT}/${dbConfig.DB}`)
-  .then(() => {
-    console.log('Successful connection to your Database!');
-    initial();
+/* ********** Mongoose Connection to MongoDB ********** */
+// db.mongoose
+//   .connect(`mongodb://${dbConfig.HOST}:${dbConfig.PORT}/${dbConfig.DB}`)
+//   .then(() => {
+//     console.log('Successful connection to your Database!');
+//     initial();
+//   })
+//   .catch((err) => {
+//     console.error('Connection error', err);
+//     process.exit();
+//   });
+
+const uri =
+  'mongodb+srv://dankal11:t4dkLHzJT!SP2RCUqxwb@cluster0.flalt.mongodb.net/Cluster0?retryWrites=true&w=majority';
+
+mongoose
+  .connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverApi: ServerApiVersion.v1,
   })
-  .catch((err) => {
-    console.error('Connection error', err);
-    process.exit();
-  });
+  .then((result) => app.listen(3001))
+  .catch((err) => console.log(err));
+
+/* ********** Routes ********** */
+// app.get('/', (req, res) => res.render('home'));
+// app.get('/smoothies', (req, res) => res.render('smoothies'));
+app.use(authRoutes);
+console.log('server started successfully! Listening on port 3001');
